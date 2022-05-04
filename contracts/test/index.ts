4d16f50874b2a2ddd1131cc7df87709cdf8a1c00
Lautarodamore <lauti.damore@gmail.com>
2022-05-03 22:53:52 -0300

    vdp collectbiles NFT MARKET

--- contracts/test/index.ts
+++ contracts/test/index.ts
@@ -1,19 +1,7 @@
 import { expect } from "chai";
-import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+describe("VdpCollectiblesNFTMarket", function () {
+  it("test", async function () {
+    expect(1).to.equal(1);
   });
 });
